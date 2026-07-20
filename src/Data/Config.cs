7326912f6e54d3f118a718a348918c0b6ec968bf
Porrith Suong <porrith.suong@gmail.com>
2026-07-20 12:59:46 -0400

    Working on a binget v2.

---
+++ src/Data/Config.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Text;
+using Tomlyn.Serialization;
+
+namespace BinGet.Data;
+
+public sealed class Config {
+    [TomlPropertyName("url")]
+    public string Url { get; set; } = string.Empty;
+    [TomlPropertyName("destination")]
+    public string Destination { get; set; } = string.Empty;
+    [TomlPropertyName("repositories")]
+    public Dictionary<string, RepositoryConfig> Repositories { get; set; }
+
+    public override string ToString() {
+        StringBuilder sb = new StringBuilder(256);
+        sb
+            .Append("Url: ")
+            .AppendLine(Url)
+            .Append("Destination: ")
+            .AppendLine(Destination);
+
+        Dictionary<string, RepositoryConfig>.Enumerator it = Repositories != null ? Repositories.GetEnumerator() : default;
+        while (it.MoveNext()) {
+            sb.AppendLine(it.Current.Key)
+                .AppendLine(it.Current.Value.ToString());
+        }
+        return sb.ToString();
+    }
+}
+
+public sealed class RepositoryConfig {
+    [TomlPropertyName("id")]
+    public string Id { get; set; } = string.Empty;
+    [TomlPropertyName("targetPattern")]
+    public string TargetPattern { get; set; } = string.Empty;
+
+    public override string ToString() {
+        return $"ID: {Id}, Target Pattern: {TargetPattern}";
+    }
+}
+
+[TomlSerializable(typeof(Config))]
+internal partial class TomlConfigContext : TomlSerializerContext { }
---
+++ src/Program.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleAppFramework;
+using ZLogger;
+using System;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using Tomlyn;
+using BinGet.Data;
+using Cysharp.IO;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace BinGet;
+
+public class Commands {
+
+    private readonly ILogger<Commands> logger;
+
+    public Commands(ILogger<Commands> logger) {
+        this.logger = logger;
+    }
+
+    /// <summary>
+    /// Parses the toml config file.
+    /// </summary>
+    /// <param name="config">-c, The configuration file to load.</param>
+    [Command("")]
+    public async Task ParseConfig(string config) {
+        if (!File.Exists(config)) {
+            throw new FileNotFoundException($"The config file: {config} does not exist!");
+        }
+        using Utf8StreamReader streamReader = new Utf8StreamReader(config, FileOpenMode.Throughput);
+        byte[] text = await streamReader.ReadToEndAsync();
+        Config output = TomlSerializer.Deserialize<Config>(Encoding.UTF8.GetString(text), TomlConfigContext.Default);
+        logger.ZLogInformation($"{output}");
+    }
+}
+
+public class Program {
+    public static void Main(string[] args) {
+        var app = ConsoleApp.Create().ConfigureLogging(static builder => {
+            builder.ClearProviders()
+                .SetMinimumLevel(LogLevel.Trace)
+                .AddZLoggerConsole()
+                .AddZLoggerFile("binget.log");
+        });
+        app.Add<Commands>();
+        app.Run(args);
+    }
+}
