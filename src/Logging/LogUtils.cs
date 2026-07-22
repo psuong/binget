c005b9ea3db5a50ceb9b4cdc70e047c5bf65c9d3
Porrith Suong <porrith.suong@gmail.com>
2026-07-22 16:43:27 -0400

    Prevents log rotation if the existing log has not been written to.

--- src/Logging/LogUtils.cs
+++ src/Logging/LogUtils.cs
@@ -26,7 +26,14 @@ public static class LogUtils {
     /// </summary>
     public static void RotateLogFiles() {
         var cwd = Directory.GetCurrentDirectory();
-        var logPath = Path.Join(cwd, LogFile);
+        var logPath = GetLogPath();
+
+        if (File.Exists(logPath)) {
+            var info = new FileInfo(logPath);
+            if (info.Length == 0) {
+                return;
+            }
+        }
 
         var backup1 = Path.Join(cwd, LogFile + ".1");
         var backup2 = Path.Join(cwd, LogFile + ".2");
